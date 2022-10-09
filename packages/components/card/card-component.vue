40855754f76ef7651848c1d1e4a76ca57fe1ad1b
Matt Sutton <matthew.c.sutton@gmail.com>
2022-10-09 09:07:02 -0600

    add base card component

---
+++ packages/components/card/card-component.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="ftw-card">
+    <div class="ftw-card__header">
+      <slot name="header">{{ header }}</slot>
+    </div>
+    <div class="ftw-card_body">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  header: String,
+});
+</script>
+
+<style scoped>
+.ftw-card {
+  padding: 20px;
+  width: 200px;
+  height: 200px;
+
+  border: 1px solid black;
+  border-radius: 5px;
+}
+</style>
